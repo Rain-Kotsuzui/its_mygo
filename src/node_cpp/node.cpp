6373824bbad8d09231db7f50d0eca6d7254a8038
RainKotsuzui <RainKotsuzui@gmail.com>
2025-05-02 18:21:07 +0800

    upload

--- src/node_cpp/node.cpp
+++ src/node_cpp/node.cpp
@@ -1,35 +1,56 @@
 #include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <stdio.h>
+#include <protocol/srv/motion_result_cmd.hpp>
 
-class DogController : public rclcpp::Node
+class BasicCmd : public rclcpp::Node
 {
 public:
-  DogController() : Node("dog_controller")
-  {
-    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    timer_ = this->create_wall_timer(
-        std::chrono::milliseconds(500), std::bind(&DogController::timer_callback, this));
-  }
+    BasicCmd(const std::string &name) : Node(name)
+    {
+        client_ = this->create_client<protocol::srv::MotionResultCmd>("/Cyberdog32/motion_result_cmd");
+        // Wait for the service server to be available
+        while (!client_->wait_for_service(std::chrono::seconds(1)))
+        {
+            if (!rclcpp::ok())
+            {
+                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service to appear.");
+                return;
+            }
+            RCLCPP_INFO(this->get_logger(), "Waiting for the service to appear...");
+        }
+    }
+
+    void send_request()
+    {
+        auto request = std::make_shared<protocol::srv::MotionResultCmd::Request>();
+        // Set the request parameters if needed
+        request->motion_id = 111;
+
+        // Send the request and get a future object
+        auto future_result = client_->async_send_request(request);
+
+        // Wait for the result
+        if (rclcpp::spin_until_future_complete(this->shared_from_this(), future_result) ==
+            rclcpp::FutureReturnCode::SUCCESS)
+        {
+            RCLCPP_INFO(this->get_logger(), "Service call succeeded.");
+            auto response = future_result.get();
+            // Process the response if needed
+        }
+        else
+        {
+            RCLCPP_ERROR(this->get_logger(), "Service call failed.");
+        }
+    }
 
 private:
-  void timer_callback()
-  {
-    auto message = geometry_msgs::msg::Twist();
-    message.linear.x = 1.0;  // 控制狗前进的速度
-
-    printf("Publishing: '%f'\n", message.linear.x);
-    publisher_->publish(message);
-  }
-
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Client<protocol::srv::MotionResultCmd>::SharedPtr client_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<DogController>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<BasicCmd>("basic_cmd_node");
+    node->send_request();
+    rclcpp::shutdown();
+    return 0;
 }
